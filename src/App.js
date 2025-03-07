688c1af27b6724f806587d10ce58128b692391c7
Nguyễn Ngọc Quân <50838399+nguyenngquan@users.noreply.github.com>
2025-03-07 21:15:46 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ const Title = styled(motion.h1)`
   transition={{ duration: 1 }}
   className="responsive-title"
 >
- Chúc mừng ngày 8/3, cún iu cụa anhhh!
+ Chúc mừng ngày 8/3 Chích Bông!
 </Title>
 
 const PuppyContainer = styled(motion.div)`
